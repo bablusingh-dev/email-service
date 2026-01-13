07eb4eedf56844aa018a9b12645b95af518ad369
Bablu Kumar Singh <bksingh3520@gmail.com>
2026-01-13 21:59:07 +0530

    feat: Added pi Key Repository in factoty repository

--- src/repositories/RepositoryFactory.ts
+++ src/repositories/RepositoryFactory.ts
@@ -1,10 +1,13 @@
 import config from '../configs/config'
 import { EApplicationEnvironment } from '../types/common.types'
+import { DrizzleApiKeyRepository } from './implementations/drizzle/DrizzleApiKeyRepository'
 import { DrizzleProjectRepository } from './implementations/drizzle/DrizzleProjectRepository'
+import { IApiKeyRepository } from './interfaces/IApiKeyRepository'
 import { IProjectRepository } from './interfaces/IProjectRepository'
 
 class RepositoryFactory {
     private static projectRepository: IProjectRepository | null = null
+    private static apiKeyRepository: IApiKeyRepository | null = null
 
     static getProjectRepository(): IProjectRepository {
         if (!this.projectRepository) {
@@ -12,9 +15,15 @@ class RepositoryFactory {
         }
         return this.projectRepository
     }
-
+    static getApiKeyRepository(): IApiKeyRepository {
+        if (!this.apiKeyRepository) {
+            this.apiKeyRepository = new DrizzleApiKeyRepository()
+        }
+        return this.apiKeyRepository
+    }
     static reset(): void {
         this.projectRepository = null
+        this.apiKeyRepository = null
     }
 
     static setProjectRepository(projectRepository: IProjectRepository): void {
@@ -22,6 +31,12 @@ class RepositoryFactory {
             throw new Error('setProjectRepository can only be used in testing environment')
         }
         this.projectRepository = projectRepository
+    }
+    static setApiKeyRepository(apiKeyRepository: IApiKeyRepository): void {
+        if (config.ENV !== EApplicationEnvironment.TESTING) {
+            throw new Error('setApiKeyRepository can only be used in testing environment')
+        }
+        this.apiKeyRepository = apiKeyRepository
     }
 }
 
